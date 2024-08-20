3708357acab946c65250a7e7d1d3c16a9dbfcacf
Haikal <haikalharin1998@gmail.com>
2024-08-20 13:30:46 +0700

    update hero

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,73 +1,82 @@
-import { Controller, Get, HttpCode, Param, Post, Redirect, Req, Res } from "@nestjs/common";
-import { ResponseUtil } from "../utils/response.util";
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { ResponseUtil } from '../utils/response.util';
 
-let heroes = [{
-  id: 1,
-  name: "Aurora",
-  type: "Mage",
-  image: "aurora.jpg"
-},
+const heroes = [
+  {
+    id: 1,
+    name: 'Aurora',
+    type: 'Mage',
+    image: 'aurora.jpg',
+  },
   {
     id: 2,
-    name: "Zilong",
-    type: "Fighter",
-    image: "zilong.jpg"
+    name: 'Zilong',
+    type: 'Fighter',
+    image: 'zilong.jpg',
   },
   {
     id: 3,
-    name: "Akai",
-    type: "Tank",
-    image: "akai.jpg"
+    name: 'Akai',
+    type: 'Tank',
+    image: 'akai.jpg',
   },
   {
     id: 4,
-    name: "bruno",
-    type: "marksman",
-    image: "bruno.jpg"
-  }];
+    name: 'bruno',
+    type: 'marksman',
+    image: 'bruno.jpg',
+  },
+];
 
-@Controller("hero")
+@Controller('hero')
 export class HeroController {
-  @Get("index")
+  @Get('index')
   @HttpCode(200)
   index(@Res() response) {
     response.json(heroes);
   }
 
-  @Get("detail/:id")
+  @Get('detail/:id')
   findOne(@Param() params: any, @Res({ passthrough: true }) response) {
     // try {
-      console.log(params.id);
+    console.log(params.id);
 
-      let data = heroes.find(object => object.id === parseInt(params.id, 10));
+    const data = heroes.find((object) => object.id === parseInt(params.id, 10));
     console.log(data);
-      if(data ) {
-
-       response.status(200);
-       return ResponseUtil.success({
-         id: data.id,
-         name: data.name,
-         type: data.type,
-         image: data.image
-       });
-     } else{
-       console.log({});
-       response.status(200);
-       return ResponseUtil.success({});
-     }
+    if (data) {
+      response.status(200);
+      return ResponseUtil.success({
+        id: data.id,
+        name: data.name,
+        type: data.type,
+        image: data.image,
+      });
+    } else {
+      console.log({});
+      response.status(200);
+      return ResponseUtil.success({});
+    }
     // }catch (error){
     //  return  ResponseUtil.error(error);
     // }
-
   }
 
-  @Get("create")
+  @Get('create')
   create(@Res({ passthrough: true }) response) {
-    response.cookie("name", "tobi");
-    return "hero create";
+    response.cookie('name', 'tobi');
+    return 'hero create';
   }
 
-  @Post("store")
+  @Post('store')
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   // @HttpCode(200)
   store(@Req() request, @Res({ passthrough: true }) response) {
@@ -83,16 +92,12 @@ export class HeroController {
       //   statusCode: "Success",
       //   data: request.body
       // };
-    } catch (error) {
-
-    }
-
+    } catch (error) {}
   }
 
-  @Get("welcome")
-  @Redirect("https://docs.nestjs.com/")
+  @Get('welcome')
+  @Redirect('https://docs.nestjs.com/')
   hello() {
-    return "welcome";
-
+    return 'welcome';
   }
 }
